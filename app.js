c91b676ba891a1f8269b005f8dce8c331330af5e
Martine Dowden <martine@dowden.us>
2017-03-22 21:54:38 -0400

    getting started

--- app.js
+++ app.js
@@ -23,8 +23,38 @@
       url: 'tutorial',
       templateUrl: 'app/modules/tutorial/tutorial.html'
     };
+    var systemSetup = {
+      name: 'systemSetup',
+      url: 'systemSetup',
+      templateUrl: 'app/modules/tutorial/systemSetup.html'
+    };
+    var systemSetup2 = {
+      name: 'systemSetup2',
+      url: 'systemSetup2',
+      templateUrl: 'app/modules/tutorial/systemSetup2.html'
+    };
+    var gettingStarted = {
+      name: 'gettingStarted',
+      url: 'gettingStarted',
+      templateUrl: 'app/modules/tutorial/gettingStarted.html'
+    };
+    var gettingStarted2 = {
+      name: 'gettingStarted2',
+      url: 'gettingStarted2',
+      templateUrl: 'app/modules/tutorial/gettingStarted2.html'
+    };
+    var gettingStarted3 = {
+      name: 'gettingStarted3',
+      url: 'gettingStarted3',
+      templateUrl: 'app/modules/tutorial/gettingStarted3.html'
+    };
     $stateProvider.state(home);
     $stateProvider.state(tutorial);
+    $stateProvider.state(systemSetup);
+    $stateProvider.state(systemSetup2);
+    $stateProvider.state(gettingStarted);
+    $stateProvider.state(gettingStarted2);
+    $stateProvider.state(gettingStarted3);
   }
 
   function States($state) {
